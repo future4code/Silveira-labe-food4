a81e4aea976add6c2a40fcbd01eb416cbf711f71
jmaraguiar <jmaraguiar22@gmail.com>
2022-05-18 14:33:05 -0300

    perfil

--- src/pages/perfil.js
+++ src/pages/perfil.js
@@ -1,8 +1,29 @@
 import React from "react"
-
+import { Container, History, Profile, Info, Address, DisplayCards } from "./Styled/PerfilStyled"
 
 export function Perfil(){
     return(
-        <h1>Perfil</h1>
+        <Container>
+            <h4>Meu Perfil</h4>
+            <Info>
+                <hr/>
+                <Profile>
+                    <h1>Perfil</h1>
+                </Profile>
+                <hr/>
+                <Address>
+                    <h1>Endereço</h1>
+                </Address>
+                <hr/>
+            </Info>
+
+            <History>
+                <h5>Histórico de pedidos</h5>
+                <hr/>
+                <DisplayCards>
+                    <h1>Cards</h1>
+                </DisplayCards>
+            </History>
+        </Container>
     )
 }
