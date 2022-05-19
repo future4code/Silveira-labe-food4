bd49d3573ae55fed5d88aff3a7fd52de38c2da76
Mariana Mendes <filhatronixmmlima@gmail.com>
2022-05-19 12:38:20 -0300

    Detalhes no signUp

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -3,13 +3,18 @@ import { useForm } from "../hooks/useForm";
 import { signUp } from "../services/user";
 import logo  from "../assets/logo.png";
 import { Button } from "@mui/material";
-import { Container, Imagem, Titulo, CenterBuntton } from "./Styled/SignupStyled"
+import { Container, Imagem, Titulo, TextPassword} from "./Styled/SignupStyled"
 import { TextField } from '@mui/material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
 export function SignUp(){
     const [form, onChange] = useForm({ name: "", email: "", cpf: "", password: ""})
     const nav = useNavigate();
     const [confirmPassword, setConfirmPassword] = useState("");
     const [loading, setLoading] = useState(false);
+    const [viewPassword, setViewPassword] = useState('password')
+
     const onSubmitForm = async (event) => {
         setLoading(true)
         event.preventDefault()
@@ -21,9 +26,19 @@ export function SignUp(){
             alert("Senhas diferentes")
         }
     }
+
     const onChangeConfirmPassword = (event) => {
         setConfirmPassword(event.target.value)
     }
+
+    const showPassword = () => {
+        if (viewPassword === 'password') {
+            setViewPassword('text')
+        } else {
+            setViewPassword('password')
+        }
+    }
+
     return(
         <div>
             <Imagem>
@@ -33,82 +48,98 @@ export function SignUp(){
                 <h3>Cadastar</h3>
             </Titulo>
             <Container>
-            <form onSubmit={onSubmitForm}>
-<TextField
-    placeholder="Nome"
-    name={"name"}
-    onChange={onChange}
-    label={"Nome"}
-    variant={'outlined'}
-    value={form.name}
-    fullWidth
-    required
-    autoFocus
-    margin="dense"
->
-</TextField>
-<TextField
-    placeholder="Email"
-    name={"email"}
-    onChange={onChange}
-    label={"email"}
-    variant={'outlined'}
-    value={form.email}
-    fullWidth
-    required
-    autoFocus
-    margin="dense"
-></TextField>
-<TextField
-    placeholder="CPF"
-    name={"cpf"}
-    onChange={onChange}
-    label={"CPF"}
-    variant={'outlined'}
-    value={form.cpf}
-    fullWidth
-    required
-    autoFocus
-    margin="dense"
-></TextField>
-<TextField
-    placeholder="Senha"
-    name={"password"}
-    onChange={onChange}
-    label={"Senha"}
-    variant={'outlined'}
-    value={form.password}
-    fullWidth
-    required
-    autoFocus
-    margin="dense" id="senha"
-    type={"password"}
-></TextField>
-<TextField
-    placeholder="Confirmar Senha"
-    name={"password"}
-    onChange={onChangeConfirmPassword}
-    label={"Confirmar Senha"}
-    variant={'outlined'}
-    value={confirmPassword}
-    fullWidth
-    required
-    autoFocus
-    margin="dense"
-    id="senhaC"
-    type={"password"}
-></TextField>
-<Button
-    style={{backgroundColor: '#E86E5A',color: 'black', fontWeight: 'bold'}}
-    loading={loading}
-    loadingPosition="end"
-    variant="contained"
-    fullWidth
-    type="submit"
->
-    Criar
-</Button>
-</form>
+                <form onSubmit={onSubmitForm}>
+                    <TextField
+                    style={{width: '100%'}}
+                    id="outlined-basic"
+                    placeholder="Nome"
+                    name={"name"}
+                    onChange={onChange}
+                    label={"Nome"}
+                    variant={'outlined'}
+                    value={form.name}
+                    fullWidth
+                    required
+                    autoFocus
+                    margin="dense"
+                    />
+                    <TextField
+                    style={{width: '100%'}}
+                    id="outlined-basic"
+                    placeholder="Email"
+                    name={"email"}
+                    onChange={onChange}
+                    label={"Email"}
+                    variant={'outlined'}
+                    value={form.email}
+                    fullWidth
+                    required
+                    autoFocus
+                    margin="dense"
+                    />
+                    <TextField
+                    style={{width: '100%'}}
+                    id="outlined-basic"
+                    placeholder="CPF"
+                    name={"cpf"}
+                    onChange={onChange}
+                    label={"CPF"}
+                    variant={'outlined'}
+                    value={form.cpf}
+                    fullWidth
+                    required
+                    autoFocus
+                    margin="dense"
+                    />
+                    <TextPassword>
+                        <TextField
+                        style={{width: '100%'}}
+                        id="outlined-basic"
+                        label="Senha"
+                        variant="outlined"
+                        name="password"
+                        value={form.password}
+                        onChange={onChange}
+                        type={viewPassword}
+                        placeholder="exemplo123"
+                        required
+                        />
+                        {(viewPassword === 'password')? 
+                            <VisibilityOffIcon className="eye" onClick={showPassword}/>
+                            :
+                            <VisibilityIcon className="eye" onClick={showPassword}/>
+                        }
+                    </TextPassword>
+                    <TextPassword>
+                        <TextField
+                        style={{width: '100%'}}
+                        id="outlined-basic"
+                        label={"Confirmar"}
+                        variant={'outlined'}
+                        name={"password"}
+                        value={confirmPassword}
+                        onChange={onChangeConfirmPassword}
+                        type={viewPassword}
+                        placeholder="Confirmar Senha"
+                        required
+                        />
+                        {(viewPassword === 'password')? 
+                            <VisibilityOffIcon className="eye" onClick={showPassword}/>
+                            :
+                            <VisibilityIcon className="eye" onClick={showPassword}/>
+                        }
+                    </TextPassword>
+                    <Button
+                    style={{backgroundColor: '#E86E5A',color: 'black', fontWeight: 'bold'}}
+                    loading={loading}
+                    loadingPosition="end"
+                    variant="contained"
+                    fullWidth
+                    type="submit"
+                    >
+                    Criar
+                    </Button>
+                </form>
             </Container>
         </div>
     )
